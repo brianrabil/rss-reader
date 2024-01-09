3891b7c643d4e1505ec9fb305dfc601c51fe9a92
Brian Rabil <brianrabil@mac.com>
2024-01-09 01:17:28 -0600

    Refactor RSS parsing and add channel retrieval

--- apps/web/app/feed/page.tsx
+++ apps/web/app/feed/page.tsx
@@ -4,7 +4,45 @@ import convert from 'xml-js'
 
 type ParsedArticle = Omit<Article, 'id' | 'createdAt' | 'updatedAt' | 'subscriptionId'>
 
+interface RSSItem {
+  title: string
+  link: string
+  guid: string
+  pubDate: Date
+  'media:content'?: string
+  description: string
+  category: string
+  'media:keywords': string[]
+  'dc:creator': string[]
+  'dc:publisher': string
+  'dc:subject': string
+}
+
 function useRSS() {
+  const getChannel = async (url: string) => {
+    'use server'
+
+    const res = await fetch(url, { method: 'GET' })
+    const content = await res.text()
+
+    return convert
+      .xml2js(content)
+      .elements.find((el) => el.name === 'rss')
+      .elements.find((el) => el.name === 'channel')
+      .elements.filter((el) => el.name !== 'item')
+      .reduce((acc, curr) => {
+        switch (curr.name) {
+          case 'atom:link':
+            acc['atomLink'] = curr.attributes
+            break
+          default:
+            acc[curr.name] = curr.elements.find((el) => el.type === 'text').text
+            break
+        }
+        return acc
+      }, {})
+  }
+
   const getFeed = async (url: string): Promise<ParsedArticle[]> => {
     'use server'
 
@@ -12,68 +50,55 @@ function useRSS() {
       method: 'GET',
     })
 
+    const content = await response.text()
+
     let articles: ParsedArticle[] = []
 
-    convert.xml2js(await response.text()).elements[0].elements[0].elements.forEach((item) => {
-      let element: Partial<{
-        title: string
-        link: string
-        guid: string
-        pubDate: Date
-        'media:content': string
-        description: string
-        category: string
-        'media:keywords': string[]
-        'dc:creator': string[]
-        'dc:publisher': string
-        'dc:subject': string
-      }> = {}
-
-      item.elements?.forEach((el) => {
-        if (!el.name) return
-        switch (el.name) {
-          case 'media:thumbnail':
-            element[el.name] = el.attributes
-            break
-          case 'media:content':
-            element[el.name] = el.attributes
-            break
-          case 'media:credit':
-            element[el.name] = el.elements[0].text
-            break
-          case 'media:keywords':
-            element[el.name] = el.elements[0].text.split(', ')
-            break
-          case 'dc:creator':
-            element[el.name] = el.elements[0].text.split(', ')
-            break
-          case 'pubDate':
-            element[el.name] = new Date(el.elements?.[0].text)
-            break
-          default:
-            element[el.name] = el.elements?.[0].text
-            break
+    convert
+      .xml2js(content)
+      .elements.find(({ name }) => name === 'rss')
+      .elements.find(({ name }) => name === 'channel')
+      .elements.filter(({ name }) => name === 'item')
+      .map(({ elements }) =>
+        elements.reduce((acc, curr) => {
+          switch (curr.name) {
+            case 'media:thumbnail':
+            case 'media:content':
+              acc[curr.name] = curr.attributes
+              break
+            case 'media:keywords':
+            case 'dc:creator':
+              acc[curr.name] = curr.elements.find((el) => el.type === 'text').text.split(', ')
+              break
+            case 'pubDate':
+              acc[curr.name] = new Date(curr.elements?.find((el) => el.type === 'text').text)
+              break
+            default:
+              acc[curr.name] = curr.elements?.find((el) => el.type === 'text').text
+              break
+          }
+          return acc
+        }, {})
+      )
+      .map((item: RSSItem) => {
+        if (item.guid && item.title && item.pubDate && item.link) {
+          articles.push({
+            guid: item.guid,
+            link: item.link,
+            title: item.title,
+            imageUrl: item['media:thumbnail']?.url ?? null,
+            pubDate: item.pubDate,
+            description: item.description ?? null,
+            content: item['media:content'] ?? null,
+            authorId: null,
+          })
         }
       })
-
-      if (element.guid && element.title && element.pubDate && element.link) {
-        articles.push({
-          guid: element.guid,
-          link: element.link,
-          title: element.title,
-          imageUrl: element['media:thumbnail']?.url ?? null,
-          pubDate: element.pubDate,
-          description: element.description ?? null,
-          content: element['media:content'] ?? null,
-          authorId: null,
-        })
-      }
-    })
 
     return articles
   }
 
-  return { getFeed }
+  return { getFeed, getChannel }
 }
 
 const prisma = new PrismaClient()
@@ -81,8 +106,9 @@ const prisma = new PrismaClient()
 const rssFeeds = { wired: 'https://www.wired.com/feed/category/gear/latest/rss' }
 
 export default async function FeedPage() {
-  const { getFeed } = useRSS()
+  const { getFeed, getChannel } = useRSS()
 
+  const channel = await getChannel(rssFeeds.wired)
   const articles = await getFeed(rssFeeds.wired)
 
   const subscriptions = await prisma.subscription.findMany({
@@ -107,17 +133,33 @@ export default async function FeedPage() {
             <a>{feed.url}</a>
             <span>{feed.lastUpdated?.toISOString()}</span>
           </div> */}
-          <div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              maxWidth: 400,
+              gap: 24,
+              fontFamily: 'sans-serif',
+            }}
+          >
             {articles.map((article) => (
-              <div key={article.guid}>
-                <div>
-                  {!!article?.imageUrl && (
-                    <img alt={`${article.title} image`} src={article?.imageUrl} />
-                  )}
+              <div
+                style={{ border: '1px solid lightgray', borderRadius: 8, overflow: 'hidden' }}
+                key={article.guid}
+              >
+                {!!article?.imageUrl && (
+                  <img
+                    alt={`${article.title} image`}
+                    src={article?.imageUrl}
+                    style={{ width: '100%', maxHeight: 200, objectFit: 'cover' }}
+                  />
+                )}
+                <div style={{ padding: '8px 16px' }}>
                   <h3>{article.title}</h3>
+                  <p style={{ fontSize: 16, lineHeight: 1.3 }}>{article.description}</p>
+
                   <h4>{article.authorId}</h4>
-                  <span>{article.pubDate?.toDateString()}</span>
-                  <p>{article.description}</p>
+                  <span style={{ fontSize: 12 }}>{article.pubDate?.toDateString()}</span>
                 </div>
                 <div>{article.content}</div>
               </div>
